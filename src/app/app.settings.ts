77c5c0189f351e43442b2b0e000be4d72b164287
joseortizdelaluz <jose.ortiz.luz@gmail.com>
2025-07-11 22:12:10 -0600

    updates to deploy to heroku

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@angular/core";
 @Injectable({ providedIn: 'root' })
 
 export class ApiSettings {
-    public static urlAPI = 'http://localhost:8000';
-    // public static urlAPI = "https://crmapi-23770faa6b83.herokuapp.com";
+    // public static urlAPI = 'http://localhost:8000';
+    public static urlAPI = "https://jks-api-520bffa4b243.herokuapp.com";
 }
